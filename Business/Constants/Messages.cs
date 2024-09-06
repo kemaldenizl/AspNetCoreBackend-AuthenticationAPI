44f50c230a6a2ee083258b6adcc834857a9eaa70
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 22:06:32 +0300

    message fields complated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,5 +8,11 @@ namespace Business.Constants
 {
     public class Messages
     {
+        public static string UserNotFound = "User not found.";
+        public static string PasswordError = "Invalid password.";
+        public static string SuccessfulLogin = "Login successful.";
+        public static string UserAlreadyExists = "This user is already exists.";
+        public static string UserRegistered = "User succesfully register.";
+        public static string AccessTokenCreated = "Access token succesfully created.";
     }
 }

82f977054d70014f741c8a8e175b5c37481b25be
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 21:42:52 +0300

    Creating context and configuring

---
+++ DataAccess/Concrete/EntityFramework/Contexts/AuthenticationDemo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class AuthenticationDemo:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-A88KP21;Database=AuthenticationDemo;Trusted_Connection=true;TrustServerCertificate=True");
+        }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}

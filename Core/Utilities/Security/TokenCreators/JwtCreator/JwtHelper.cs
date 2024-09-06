c0ea14aef173352c765c8db2fa2be88d6e261fa2
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 18:41:05 +0300

    Creating JwtHelper who implemented ITokenHelper and implementing ITokenHelper's abstract methods

---
+++ Core/Utilities/Security/TokenCreators/JwtCreator/JwtHelper.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using Core.Utilities.Security.TokenEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.TokenCreators.JwtCreator
+{
+    public class JwtHelper<TUser, TOperationClaim> : ITokenHelper<TUser, TOperationClaim>
+        where TUser : class, IEntity, new()
+        where TOperationClaim : class, IEntity, new()
+    {
+        public AccessToken CreateToken(TUser user, List<TOperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

d2dedc7d9523c001af6c0bd3c8cb0cdeff7f29ad
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 19:39:29 +0300

    Starting coding SetClaims and taking prop values of TUser with reflection

--- Core/Utilities/Security/TokenCreators/JwtCreator/JwtHelper.cs
+++ Core/Utilities/Security/TokenCreators/JwtCreator/JwtHelper.cs
@@ -4,15 +4,15 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Utilities.Security.TokenCreators.JwtCreator
 {
-    public class JwtHelper<TUser, TOperationClaim, TClaim> : ITokenHelper<TUser, TOperationClaim>
+    public class JwtHelper<TUser, TOperationClaim> : ITokenHelper<TUser, TOperationClaim>
         where TUser : class, IEntity, new()
         where TOperationClaim : class, IEntity, new()
-        where TClaim : class, IEntity, new()
     {
         public IConfiguration Configuration { get; }
         private TokenOptions _tokenOptions;
@@ -24,9 +24,14 @@ namespace Core.Utilities.Security.TokenCreators.JwtCreator
             _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
         }
 
-        private IEnumerable<TClaim> SetClaims(TUser user, List<TOperationClaim> operationClaims)
+        private IEnumerable<Claim> SetClaims(TUser user, List<TOperationClaim> operationClaims)
         {
+            var id = typeof(TUser).GetProperty("Id")?.GetValue(user)?.ToString();
+            var email = typeof(TUser).GetProperty("Email")?.GetValue(user)?.ToString();
+            var firstname = typeof(TUser).GetProperty("FirstName")?.GetValue(user)?.ToString();
+            var lastname = typeof(TUser).GetProperty("LastName")?.GetValue(user)?.ToString();
 
+            
         }
 
         public AccessToken CreateToken(TUser user, List<TOperationClaim> operationClaims)

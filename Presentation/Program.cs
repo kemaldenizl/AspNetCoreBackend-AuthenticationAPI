a65435cf546332c864e1903febca270f74043f0b
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 23:26:56 +0300

    completing configuration of token validation

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,8 @@
 
+using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.TokenEntities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Presentation
 {
@@ -14,7 +16,21 @@ namespace Presentation
 
             var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
+                    };
+                });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

a6aa6395c3f1f8264eb35ba683a26f7836280d26
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 23:45:47 +0300

    Completing giving object to dependency injections

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Security.TokenCreators;
+using Core.Utilities.Security.TokenCreators.JwtCreator;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<JwtHelper<User,OperationClaim>>().As<ITokenHelper<User,OperationClaim>>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+        }
+    }
+}

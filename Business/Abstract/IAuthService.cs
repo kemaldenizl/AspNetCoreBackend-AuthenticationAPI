52a47cc434bfd9e807a2f475ace73e07b001c4f9
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 22:25:48 +0300

    Complate abstract methods for IAuthServices

--- Business/Abstract/IAuthService.cs
+++ Business/Abstract/IAuthService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Utilities.Results.Abstract;
+using Core.Utilities.Security.TokenEntities;
+using Entities.Concrete;
+using Entities.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,9 @@ namespace Business.Abstract
 {
     public interface IAuthService
     {
-        
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExist(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
     }
 }

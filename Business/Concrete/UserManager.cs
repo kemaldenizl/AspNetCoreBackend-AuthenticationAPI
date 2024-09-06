e9e2f03f939893b81fd3562510d0f7a1b5fd1685
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 22:17:03 +0300

    implementing missing members of IUserService to UserManager

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,21 @@ namespace Business.Concrete
         public UserManager(IUserDal userDal)
         {
             _userDal = userDal;
+        }
+
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByMail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            throw new NotImplementedException();
         }
     }
 }

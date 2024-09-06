7fb6b0d78d0c758318e9380d96fbda9c8c0e7bb3
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 20:22:25 +0300

    complete CreatePasswordHash method who takes password and giving us passwordHash and passwordSalt using -call by reference- out keyword

--- Core/Utilities/Security/Hashing/HashingHelper.cs
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -8,5 +8,13 @@ namespace Core.Utilities.Security.Hashing
 {
     public class HashingHelper
     {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); 
+            }
+        }
     }
 }

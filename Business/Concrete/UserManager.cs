ec998439553defbb9d3969f9c8017da828894a67
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 22:16:18 +0300

    Dependency Injection for can be use userDal in userManager

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,11 @@ namespace Business.Concrete
 {
     public class UserManager : IUserService
     {
+        private IUserDal _userDal;
 
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
     }
 }

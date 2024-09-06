5f8ca03b64da0b1ae09b0f90b3747c6bdbf1e1bb
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 22:32:01 +0300

    Starting with IUserService and ITokenHelper dependency injection to coding AuthManager

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Core.Utilities.Security.TokenCreators;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AuthManager:IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper<User,OperationClaim> _tokenHelper;
+
+        public AuthManager(IUserService userService, ITokenHelper<User, OperationClaim> tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+    }
+}

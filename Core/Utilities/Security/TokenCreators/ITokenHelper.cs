46c38ab3851634847d138746b606145b9099aabf
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-09-06 18:33:32 +0300

    Starting Develop ITokenHelper for generic Token creating and writing some conditions for generic entities

---
+++ Core/Utilities/Security/TokenCreators/ITokenHelper.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Abstract;
+using Core.Utilities.Security.TokenEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.TokenCreators
+{
+    public interface ITokenHelper<TUser,TOperationClaim> 
+        where TUser : class, IEntity, new() 
+        where TOperationClaim : class, IEntity, new()
+    {
+        
+    }
+}
